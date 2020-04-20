f24f9a74d3f7549c59d905db553baa858b40a6fd
Michael Brown <mbrown@fensystems.co.uk>
2020-04-20 14:57:01 +0100

    python: Allow building against in-tree or externally installed C library
    Extend the build_ext command to allow for using either:
    
    - an in-tree copy of the libcx C library, with the autotools-based
      build triggered automatically before attempting to build the Python
      extension, or
    
    - an externally installed copy of the libcx C library, detected via
      pkg-config.
    
    The in-tree copy is used by default if building from a source tree
    that includes the C library.  This can be overridden on demand using
    the "--extlibcx" option.
    
    Also use the pkg-config infrastructure to link directly against
    OpenSSL since the Python extension does call into OpenSSL directly (as
    well as indirectly via libcx).
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- python/setup.py
+++ python/setup.py
@@ -3,20 +3,81 @@
 """Setup script"""
 
 from pathlib import Path
+import subprocess
 from setuptools import Extension, setup, find_packages
+from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 
-cdir = Path('../c').resolve()
-incdir = cdir / 'include'
-libdir = cdir / 'src' / '.libs'
 
-extensions = [Extension(
-    '*', ['libcx/*.pyx'],
-    libraries=['cx'],
-    include_dirs=[str(incdir)],
-    library_dirs=[str(libdir)],
-    runtime_library_dirs=[str(libdir)],
-)]
+class BuildExtCommand(build_ext):
+    """Custom build_ext command"""
+
+    user_options = build_ext.user_options + [
+        ('extlibcx', None, "Force use of externally installed libcx"),
+    ]
+    boolean_options = build_ext.boolean_options + [
+        'extlibcx',
+    ]
+
+    def initialize_options(self):
+        """Initialize options"""
+        super().initialize_options()
+        self.extlibcx = not Path('../c/src/generator.c').exists()
+
+    @staticmethod
+    def pkgconf(libname, *args):
+        """Run pkg-config and capture output"""
+        cmd = ['pkg-config', libname, *args]
+        return subprocess.check_output(cmd).decode().split()
+
+    @classmethod
+    def pkgconf_vals(cls, libname, *args):
+        """Run pkg-config, capture output, and strip initial -<X>"""
+        return [x[2:] for x in cls.pkgconf(libname, *args)]
+
+    def pkgconf_ext(self, libname):
+        """Run pkg-config to get extension attributes"""
+        return {
+            'include_dirs': self.pkgconf_vals(libname, '--cflags-only-I'),
+            'library_dirs': self.pkgconf_vals(libname, '--libs-only-L'),
+            'libraries': self.pkgconf_vals(libname, '--libs-only-l'),
+            'extra_compile_args': self.pkgconf(libname, '--cflags-only-other'),
+            'extra_link_args': self.pkgconf(libname, '--libs-only-other'),
+        }
+
+    def build_libcx(self):
+        """Build libcx C library and return extension attributes"""
+        cxdir = Path('../c').resolve()
+        if not (cxdir / 'configure').exists():
+            subprocess.check_call('./autogen.sh', cwd=cxdir)
+        if not (cxdir / 'Makefile').exists():
+            subprocess.check_call('./configure', cwd=cxdir)
+        subprocess.check_call('make', cwd=cxdir)
+        incdir = str(cxdir / 'include')
+        libdir = str(cxdir / 'src/.libs')
+        return {
+            'include_dirs': [incdir],
+            'library_dirs': [libdir],
+            'runtime_library_dirs': [libdir],
+            'libraries': ['cx'],
+        }
+
+    @staticmethod
+    def merge_attributes(ext, params):
+        """Merge extension parameters"""
+        for name, values in params.items():
+            attr = getattr(ext, name)
+            attr.extend(x for x in values if x not in attr)
+
+    def run(self):
+        """Run command"""
+        cx = self.pkgconf_ext('cx') if self.extlibcx else self.build_libcx()
+        openssl = self.pkgconf_ext('openssl')
+        for ext in self.extensions:
+            self.merge_attributes(ext, cx)
+            self.merge_attributes(ext, openssl)
+        super().run()
+
 
 setup(
     long_description=open('README.md').read(),
@@ -24,6 +85,9 @@ setup(
     packages=find_packages(exclude=['test']),
     use_scm_version={'root': '..'},
     python_requires='>=3.7',
+    cmdclass={
+        'build_ext': BuildExtCommand,
+    },
     setup_requires=[
         'Cython',
         'setuptools_scm',
@@ -31,7 +95,7 @@ setup(
     install_requires = [
         'pyOpenSSL',
     ],
-    ext_modules=cythonize(extensions, language_level=3, annotate=True),
+    ext_modules=cythonize('libcx/*.pyx', language_level=3, annotate=True),
     test_suite='test',
     zip_safe=False,
 )
