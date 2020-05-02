7b4f2bb81684af944c0de8ca74eb30d47a118563
Michael Brown <mbrown@fensystems.co.uk>
2020-05-02 18:04:59 +0100

    kotlin: Build OpenSSL as an Android native library
    Build OpenSSL using the Android NDK via the externalNativeBuild
    mechanism.
    
    The Kotlin Multiplatform Gradle plugin is unfortunately not designed
    according to the Unix philosophy of "do one thing and do it well"; it
    is a huge and lumbering monolith.  Though it does provide the ability
    to compile Android native code via the NDK (using a copy of the
    Android NDK that is completely unrelated to the copy used by the
    Android Gradle plugin), it provides no sensible way to retrieve the
    locations of these downloaded tools.
    
    The older Kotlin Native Gradle plugin did provide clean ways to access
    the individual NDK tools.  Unfortunately, this plugin is now
    deprecated and has a package namespace collision with the
    substantially less friendly Kotlin Multiplatform Gradle plugin.
    
    The upshot is that there is no clean way to exploit Kotlin
    Multiplatform's capability to automatically download appropriate
    toolchains for each supported target architecture.  Provide instead an
    Android-only mechanism using CMake to drive the OpenSSL build process.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -10,12 +10,18 @@ repositories {
 }
 
 android {
+    ndkVersion = "21.0.6113669"
     compileSdkVersion(29)
     sourceSets {
 	val main by getting {
 	    manifest.apply {
 		srcFile("src/androidMain/AndroidManifest.xml")
 	    }
+	}
+    }
+    externalNativeBuild {
+	cmake {
+	    setPath("src/androidMain/cpp/CMakeLists.txt")
 	}
     }
 }
---
+++ kotlin/src/androidMain/cpp/dummy.c
@@ -0,0 +1,4 @@
+/*
+ * Dummy file required to trick Gradle into building the external libraries
+ *
+ */
